8b1705c19eec1d71471b2e4612f9da8f37b64288
orange <masoncaleb779@gmail.com>
2026-08-29 14:27:46 -0400

    rev2

--- yt-dlpw.bat
+++ yt-dlpw.bat
@@ -35,22 +35,24 @@ goto start
 
 :audio
 cls
+echo audio menu
 set /p URL=VIDEO URL HERE:
 echo.
-appdata/yt-dlp-batch-v1.1.0/yt-dlp.exe -x --audio-format mp3 --audio-quality 0 --embed-thumbnail %URL%
+appdata/yt-dlp-batch-v1.1.0/yt-dlp.exe ..\..\YTDLP batch script app i made so cool yea\downloads -x --audio-format mp3 --audio-quality 0 --embed-thumbnail %URL%
 echo.
 goto close
 
 :video
 cls
+echo video menu
 set /p URL=VIDEO URL HERE:
 echo.
-appdata/yt-dlp-batch-v1.1.0/yt-dlp.exe -f "bv*[height<=1080][ext=mp4]+ba[ext=m4a]/b[height<=1080][ext=mp4]" --merge-output-format mp4 %URL%
+appdata/yt-dlp-batch-v1.1.0/yt-dlp.exe -o ..\..\YTDLP batch script app i made so cool yea\downloads  -f "bv*[height<=1080][ext=mp4]+ba[ext=m4a]/b[height<=1080][ext=mp4]" --merge-output-format mp4 %URL%
 echo.
 goto close
 
 :close
-echo close
+echo press enter to close
 pause
 exit
 
