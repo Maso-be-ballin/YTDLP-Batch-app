8cdbfeb15b142d784d4ffe8ed06566327a7218e5
orange <masoncaleb779@gmail.com>
2026-08-30 02:25:28 -0400

    fix filenames2

--- yt-dlpw.bat
+++ yt-dlpw.bat
@@ -38,16 +38,16 @@ cls
 echo audio menu
 set /p URL=VIDEO URL HERE:
 echo.
-"appdata\yt-dlp-batch-v1.1.0\yt-dlp.exe" -P "downloads\audio" -o "%(title)s.%(ext)s" -x --audio-format mp3 --audio-quality 192 --embed-thumbnail %URL%
+"appdata\yt-dlp-batch-v1.1.0\yt-dlp.exe" -P "downloads\audio" -o "%%(title)s.%%(ext)s" -x --audio-format mp3 --audio-quality 192 --embed-thumbnail --embed-metadata %URL%
 echo.
 goto close
 
 :video
 cls
 echo video menu
-set /p URL=VIDEO URL HERE:
+set /p "URL=VIDEO URL HERE: "
 echo.
-"appdata\yt-dlp-batch-v1.1.0\yt-dlp.exe" -P "downloads\video" -o "%(title)s.%(ext)s" -f "bv*[height<=1080][ext=mp4]+ba[ext=m4a]/b[height<=1080][ext=mp4]" --merge-output-format mp4 %URL%
+"appdata\yt-dlp-batch-v1.1.0\yt-dlp.exe" -P "downloads\video" -o "%%(title)s.%%(ext)s" -f "bv*[height<=1080][ext=mp4]+ba[ext=m4a]/b[height<=1080][ext=mp4]" --merge-output-format mp4 --embed-thumbnail --embed-metadata %URL%
 echo.
 goto close
 
